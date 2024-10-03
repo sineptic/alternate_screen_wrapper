39ca83e82f94355012e62bdfe68f9b1cc06e85cb
sineptic <sineptic0@gmail.com>
2024-10-03 08:50:20 +0300

    add bracket_paste_mode

--- src/crossterm.rs
+++ src/crossterm.rs
@@ -1,28 +1,50 @@
 use crossterm::{
+    event::{DisableBracketedPaste, EnableBracketedPaste},
+    execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    ExecutableCommand,
 };
-use std::io::stdout;
+use std::{
+    io::stdout,
+    panic::{set_hook, take_hook},
+};
 
 #[non_exhaustive]
 pub struct AlternateScreen {}
 impl AlternateScreen {
     pub fn enter() -> std::io::Result<Self> {
-        stdout().execute(EnterAlternateScreen)?;
+        init_panic_hook();
+        execute!(stdout(), EnterAlternateScreen)?;
         enable_raw_mode()?;
         Ok(Self {})
+    }
+    pub fn bracketed_paste(self) -> std::io::Result<Self> {
+        execute!(stdout(), EnableBracketedPaste)?;
+        Ok(self)
     }
 }
 impl Drop for AlternateScreen {
     fn drop(&mut self) {
-        let err1 = stdout().execute(LeaveAlternateScreen).map(|_| ());
-        let err2 = disable_raw_mode();
-
-        if let Err(err) = err1 {
-            eprintln!("Error: {err}.");
-        }
-        if let Err(err) = err2 {
-            eprintln!("Error: {err}.");
+        if let Err(err) = restore_tui() {
+            eprintln!("Error: {err}");
         }
     }
+}
+
+fn restore_tui() -> std::io::Result<()> {
+    disable_raw_mode()?;
+    execute!(
+        std::io::stdout(),
+        LeaveAlternateScreen,
+        DisableBracketedPaste
+    )?;
+    Ok(())
+}
+
+fn init_panic_hook() {
+    let original_hook = take_hook();
+    set_hook(Box::new(move |panic_info| {
+        // intentionally ignore errors here since we're already in a panic
+        let _ = restore_tui();
+        original_hook(panic_info);
+    }));
 }
